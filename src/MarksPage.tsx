2583d8a8d49335d96590ba57d4006b3a67f694a5
薄井百花 <momoyama@Macintosh.local>
2022-05-26 14:49:25 +0900

    firebase 変更 5/26

--- src/MarksPage.tsx
+++ src/MarksPage.tsx
@@ -1,11 +1,14 @@
 
 import db from './firebase'
 import { collection, getDocs } from 'firebase/firestore'
-import { CoordinateData } from "./types";
+import { CoordinateData, Position } from "./types";
 import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
-import Geocode from "react-geocode";
 import { useEffect, useState } from "react";
 import { API_KEY } from "./config"
+
+
+
+
 
 
 const MarksPage = () => {
@@ -18,23 +21,29 @@ const MarksPage = () => {
         height: '600px'
     };
 
+
     const [markers, setMarkers] = useState<CoordinateData[]>([])
 
     useEffect(() => {
         //データベースからデータを取得する
         const markerData = collection(db, "markers");
         getDocs(markerData).then((snapShot) => {
-            console.log(snapShot.docs.map((doc) => doc.data())[0]);
+            // console.log(snapShot.docs.map((doc) => doc.data())[0]);
             setMarkers([...snapShot.docs.map((doc) => {
                 return {
                     latitude: doc.data().latitude,
                     longitude: doc.data().longitude
                 }
+
             })]);
 
         })
 
     }, [])
+
+
+
+    const pins: { lat: number, lng: number }[] = [];
 
 
     return isLoaded ? (
@@ -43,18 +52,33 @@ const MarksPage = () => {
             <div>
                 {
                     markers.map((mark, key) => {
+
+                        const apple: Position = { lat: mark.latitude, lng: mark.longitude }
+                        pins.push(apple);
+
+
                         return (
                             <div key={key}>
+
                                 <h1>latitude: {mark.latitude}</h1>
                                 <h1>longitude: {mark.longitude}</h1>
+
+
                             </div>
                         );
                     })}
             </div>
+
             <GoogleMap mapContainerStyle={containerStyle} center={{ lat: 35.7056396, lng: 139.7518913 }} zoom={17}>
 
-                <Marker position={{ lat: 35.5687398, lng: 139.3950611 }} />
-                <Marker position={{ lat: 35.5687398, lng: 139.3950611 }} />
+                {
+                    pins.map((pin) => {
+                        console.log(pin)
+
+                        return (<><Marker position={pin} /></>);
+                    })}
+
+
             </GoogleMap>
         </>
 
--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,10 @@ export type CoordinateData = {
     latitude: number
     longitude: number
 }
+
+
+export type Position = {
+    lat: number
+    lng: number
+
+}
